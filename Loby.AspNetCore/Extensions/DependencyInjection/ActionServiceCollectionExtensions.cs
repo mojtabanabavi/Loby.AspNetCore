ec87f38f98d10dc29f99bc9d34a6e966a4f7efc7
Mojtaba Nabavi <dev.nabavi@gmail.com>
2022-06-20 21:15:08 +0430

    Adding action context accessor registry extension for service collection

---
+++ Loby.AspNetCore/Extensions/DependencyInjection/ActionServiceCollectionExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace Loby.AspNetCore.Extensions.DependencyInjection
+{
+    public static class ActionServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds a default implementation for the <see cref="IActionContextAccessor"/> service.
+        /// </summary>
+        /// <param name="services">
+        /// The <see cref="IServiceCollection"/>.
+        /// </param>
+        /// <returns>
+        /// The service collection.
+        /// </returns>
+        public static IServiceCollection AddActionContextAccessor(this IServiceCollection services)
+        {
+            if (services == null)
+            {
+                throw new ArgumentNullException(nameof(services));
+            }
+
+            services.TryAddSingleton<IActionContextAccessor, ActionContextAccessor>();
+
+            return services;
+        }
+    }
+}
